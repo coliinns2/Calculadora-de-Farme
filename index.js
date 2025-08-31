68ebe2343c621caacab22dc8361d7e4a21e3177a
coliinns2 <atendimento.eucatur.transportes@gmail.com>
2025-08-31 12:25:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -618,7 +618,7 @@ client.on('ready', () => {
 });
 
 // --- Cron job: apagar relatórios semanais todo domingo às 19h ---
-cron.schedule('53 11 * * 0', async () => {
+cron.schedule('35 12 * * 0', async () => {
     const reportChannel = await client.channels.fetch(REPORT_CHANNEL_ID).catch(() => null);
     if (!reportChannel) return;
 
@@ -661,3 +661,4 @@ app.listen(PORT, () => {
 });
 
 client.login(DISCORD_TOKEN);
+
